990e40884bc04d78fb0931423a883631fbe9db26
Jesse R. Larsen <larsenj732@gmail.com>
2020-10-08 21:39:14 -0400

    added jarret code

--- script.js
+++ script.js
@@ -42,11 +42,11 @@ $(function () {
 
         var pokemonHeight = $("<p>").text("Height: " + response.height + "in");
         console.log(response.height);
-        $(".poke-img").append(pokemonHeight);
+        $(".poke-sprite").append(pokemonHeight);
 
         var pokemonWeight = $("<p>").text("Weight: " + response.weight + "lb");
         console.log(response.weight);
-        $(".poke-img").append(pokemonWeight);
+        $(".poke-sprite").append(pokemonWeight);
 
 
         // then i want to appened abilities of pokemon
@@ -119,8 +119,12 @@ $(function () {
         $(".pokemon-stats").append(pokeStatSpeedLi)
 
         //then i want to append pokemon type
-        console.log(response)
-        console.log(response.species.name)
+        var pokemonType = response.types[0].type.name;
+        var pokeStatTypeLi = $('<li>');
+        pokeStatTypeLi.text( 'Type: ' +pokemonType);
+        $(".pokemon-types").append(pokeStatTypeLi)
+
+        console.log(pokemonType)
 
         pokemonGo(pokemon);
       });
@@ -135,7 +139,7 @@ $(function () {
         var pokeImage = $("<img>");
         pokeImage.attr("src", imageUrl);
         pokeImage.attr("alt", "pokemon image")
-        $(".poke-img").append(pokeImage);
+        $(".poke-gif").append(pokeImage);
         console.log(response)
         console.log(response.data)
       });
