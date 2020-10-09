8622fa63f862227412be425260208b9cff0bf294
Krista <krista.coding@gmail.com>
2020-10-08 20:40:03 -0400

    pokemon items

--- script.js
+++ script.js
@@ -1,88 +1,144 @@
-alert('connected!')
 $(function () {
 
   $("#search-input").on("click", function () {
-
     var pokemon = $("#input").val().toLowerCase().trim();
+    pokemonApp(pokemon);
     
+    
+  })  
+
+  function pokemonApp(pokemon) {
+
     var pokeGame = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
     //var pokeDescription = "https://pokeapi.co/api/v2/characteristic/" + pokeid;
     var pokeGif = "https://api.giphy.com/v1/gifs/search?api_key=I4lc5z5Pjva79mvZq3suogFUfDYu5RbM&q=" + pokemon; "&limit=1&offset=0&rating=g&lang=en";
 
-    function pokemonApp() {
+    console.log(pokeGame);
+    console.log(pokemon);
 
-      console.log(pokeGame);
-      console.log(pokemon);
-
-      $.ajax({
-        url: pokeGame,
-        method: "GET"
-      })
-
-        .then(function (response) {
-
-          console.log(response)
-
-          if (!(history.includes(pokemon))) {
-            history.push(pokemon);
-            localStorage.setItem("history", JSON.stringify(history));
-            historyRow(pokemon);
-          }
-
-
-          var pokeName = response.species.name;
-          var pokeHeader = $("<h1>");
-          pokeHeader.text(pokeName);
-          $(".pokemon-name").append(pokeHeader);
-          // then i want to appened description of pokemon
-          // then i want to append top 5 moves to page
-          // then i want to append stat names
-          // then i want to appened stat values
-          //then i want to append pokemon type
-          console.log(response)
-          console.log(response.species.name)
-        });
-      $.ajax({
-        url: pokeGif,
-        method: "GET"
-      })
-
-        .then(function (response) {
-          var imageUrl = response.data[1].images.original.url;
-          var pokeImage = $("<img>");
-          pokeImage.attr("src", imageUrl);
-          pokeImage.attr("alt", "pokemon image")
-          $(".poke-img").append(pokeImage);
-          console.log(response)
-          console.log(response.data)
-        });
-
-    }
-
-    var history = JSON.parse(localStorage.getItem("history")) || [];
-    console.log(history);
-
-    if (history.length >= 0) {
-      pokemonApp(history[history.length - 1]);
-    }
-
-    for (var i = 0; i < history.length; i++) {
-      historyRow(history[i]);
-
-    }
-
-    $("#local-storage").on("click", "button", function () {
-      var pokemon1 = $(this).text().trim();
-      pokemonApp(pokemon1);
+    $.ajax({
+      url: pokeGame,
+      method: "GET"
     })
 
-    function historyRow(text) {
-      var listitem = $("<button>").text(text);
-      listitem.addClass("button is-warning");
-      $("#local-storage").append(listitem);
+      .then(function (response) {
+
+        console.log(response)
+
+        if (!(history.includes(pokemon))) {
+          history.push(pokemon);
+          localStorage.setItem("history", JSON.stringify(history));
+          historyRow(pokemon);
+        }
+
+
+        var pokeName = response.species.name;
+        var pokeHeader = $("<h1>");
+        pokeHeader.text(pokeName);
+        $(".pokemon-name").append(pokeHeader);
+
+
+        // pokemon height and weight          
+
+        var pokemonHeight = $("<p>").text("Height: " + response.height + "in");
+        console.log(response.height);
+        $(".poke-img").append(pokemonHeight);
+
+        var pokemonWeight = $("<p>").text("Weight: " + response.weight + "lb");
+        console.log(response.weight);
+        $(".poke-img").append(pokemonWeight);
+
+
+        // then i want to appened abilities of pokemon
+        var titleAbilities = $("<h2>").text("Abilities");
+        $(".pokemon-abilities").append(titleAbilities);
+        for (var i = 0; i < response.abilities.length; i++) {
+          var pokeAbilities = $("<li>").text(response.abilities[i].ability.name);
+          console.log(pokeAbilities);
+          $(".pokemon-abilities").append(pokeAbilities);
+        }
+
+
+        // then i want to append top 5 moves to page
+        var titleMoves = $("<h2>").text("Moves");
+        $(".pokemon-moves").append(titleMoves)
+        for (var i = 0; i < 5; i++) {
+          var pokemoves = $("<li>").text(response.moves[i].move.name);
+          console.log(pokemoves);
+          $(".pokemon-moves").append(pokemoves);
+        }
+
+        // then i want to append stat names
+        // then i want to appened stat values
+        //then i want to append pokemon type
+        console.log(response)
+        console.log(response.species.name)
+
+        pokemonGo(pokemon);
+      });
+
+    $.ajax({
+      url: pokeGif,
+      method: "GET"
+    })
+
+      .then(function (response) {
+        var imageUrl = response.data[1].images.original.url;
+        var pokeImage = $("<img>");
+        pokeImage.attr("src", imageUrl);
+        pokeImage.attr("alt", "pokemon image")
+        $(".poke-img").append(pokeImage);
+        console.log(response)
+        console.log(response.data)
+      });
+
+
+  };
+
+
+
+  function pokemonGo(pokemon){
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "pokemon-go1.p.rapidapi.com",
+        "x-rapidapi-key": "b74f32c4f3msh03893b8e16afef9p102c32jsn1de9b6fcf0a5"
+      }
     }
+  
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+  
+ 
+  
+    });
+  }
 
 
-  });
+  var history = JSON.parse(localStorage.getItem("history")) || [];
+  console.log(history);
+
+  if (history.length >= 0) {
+    pokemonApp(history[history.length - 1]);
+  }
+
+  for (var i = 0; i < history.length; i++) {
+    historyRow(history[i]);
+
+  }
+
+  $("#local-storage").on("click", "button", function () {
+    var pokemon1 = $(this).text().trim();
+    pokemonApp(pokemon1);
+  })
+
+  function historyRow(text) {
+    var listitem = $("<button>").text(text);
+    listitem.addClass("button is-warning");
+    $("#local-storage").append(listitem);
+  }
 });
 
